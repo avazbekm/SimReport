f15a390977b404e1f843bd9e114e3ed5e33261d4
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-11-19 22:31:50 +0500

    UI is being written

--- SimReport/MainWindow.xaml.cs
+++ SimReport/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SimReport
 {
@@ -23,6 +10,27 @@ namespace SimReport
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximazi_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized) 
+                this.WindowState = WindowState.Normal;
+            else this.WindowState = WindowState.Maximized;
+        }
+
+        private void btnMinimazed_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void brDragable_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
