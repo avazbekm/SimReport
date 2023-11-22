ac06305cea40125e87f609c34ce779ffc84d0d01
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-11-22 13:19:59 +0500

    changes added

--- SimReport/Windows/Clients/ClientCreateWindow.xaml.cs
+++ SimReport/Windows/Clients/ClientCreateWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using SimReport.Windows.Clients;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SimReport.Pages
+namespace SimReport.Windows.Clients
 {
     /// <summary>
-    /// Interaction logic for ClientsPage.xaml
+    /// Interaction logic for ClientCreateWindow.xaml
     /// </summary>
-    public partial class ClientsPage : Page
+    public partial class ClientCreateWindow : Window
     {
-        public ClientsPage()
+        public ClientCreateWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnCreate_Click(object sender, RoutedEventArgs e)
-        {
-            ClientCreateWindow clientCreateWindow = new ClientCreateWindow();
-            clientCreateWindow.ShowDialog();
         }
     }
 }
