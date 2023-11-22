e032485340e6b80ac0790d2b468d1a88dfd285bf
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-11-22 22:43:50 +0500

    changes added

--- SimReport/Entities/Companies/Company.cs
+++ SimReport/Entities/Companies/Company.cs
@@ -7,4 +7,5 @@ public class Company : Auditable
     [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
     public int AssetId { get; set; }
+    public string Color { get; set; } = string.Empty;
 }
